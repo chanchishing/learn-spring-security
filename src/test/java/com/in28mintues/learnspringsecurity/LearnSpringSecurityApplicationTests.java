6f4b6b9223b1210796479ec11d83d64a3e031613
shing <chishing@netvigator.com>
2023-03-17 20:35:23 +0800

    Initial commit

---
+++ src/test/java/com/in28mintues/learnspringsecurity/LearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28mintues.learnspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
