3facdfe63f2e6c5be7e3700225602749e098c12f
shing <chishing@netvigator.com>
2023-03-22 21:04:17 +0800

    prepare to use JWT Authentication, work in progress

--- src/main/java/com/in28mintues/learnspringsecurity/jwt/JwtAuthSecurityConfiguration.java
+++ src/main/java/com/in28mintues/learnspringsecurity/jwt/JwtAuthSecurityConfiguration.java
@@ -1,25 +1,26 @@
-package com.in28mintues.learnspringsecurity.basic;
+package com.in28mintues.learnspringsecurity.jwt;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+@Configuration
+public class JwtAuthSecurityConfiguration {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,26 +40,10 @@ public class BasicAuthSecurityConfiguration {
         //for h2 console using HTML frames
         http.headers().frameOptions().sameOrigin();
 
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
         return http.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//
-//        var user=User.withUsername("in28minutes")
-//                        .password("{noop}dummy")
-//                        .roles("USER")
-//                        .build();
-//        //aW4yOG1pbnV0ZXM6ZHVtbXk=
-//
-//        var admin=User.withUsername("admin")
-//                .password("{noop}dummy")
-//                .roles("ADMIN")
-//                .build();
-//        //YWRtaW46ZHVtbXk=
-//
-//        return new InMemoryUserDetailsManager(user,admin);
-//    }
 
     @Bean
     public DataSource dataSource(){
@@ -97,5 +82,17 @@ public class BasicAuthSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordBCryptEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        JwtDecoder JwtDecoder= new JwtDecoder() {
+            @Override
+            public Jwt decode(String token) throws JwtException {
+                return null;
+            }
+        };
+        return JwtDecoder;
     }
 }
