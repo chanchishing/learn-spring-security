571f104d29f016d2ccca2c48c4e112eea8470874
chanchishing <chishing@netvigator.com>
2023-03-22 10:29:28 +0800

    enable Basic Authentication, disable CSRF, disable Form Login, enable STATELESS session Management

---
+++ src/main/java/com/in28mintues/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.in28mintues.learnspringsecurity.basic;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->{
+           auth.anyRequest().authenticated();
+        });
+        http.sessionManagement(session->{
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
+
+        http.httpBasic();
+
+        http.formLogin().disable();
+
+        http.csrf().disable();
+
+        return http.build();
+    }
+}
