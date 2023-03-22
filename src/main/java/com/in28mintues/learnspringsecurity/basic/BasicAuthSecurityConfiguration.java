25442d5dcb56bd845e7da6d31f93e66a5729456c
chanchishing <chishing@netvigator.com>
2023-03-22 13:00:42 +0800

    storing credentials in memory

--- src/main/java/com/in28mintues/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28mintues/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,5 +30,23 @@ public class BasicAuthSecurityConfiguration {
         http.csrf().disable();
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsSErvice() {
+
+        var user=User.withUsername("in28minutes")
+                        .password("{noop}dummy")
+                        .roles("USER")
+                        .build();
+        //aW4yOG1pbnV0ZXM6ZHVtbXk=
+
+        var admin=User.withUsername("admin")
+                .password("{noop}dummy")
+                .roles("ADMIN")
+                .build();
+        //YWRtaW46ZHVtbXk=
+
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
